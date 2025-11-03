e9525e571c7c8a4964ad5bda1615d9a9481d66a8
Jacob Van De Weert <jacob@foursquare.com>
2025-11-03 13:46:22 -0500

    remove scala.reflect.runtime.universe from default package map

--- jvm/constants.go
+++ jvm/constants.go
@@ -17,12 +17,9 @@ var (
 	)
 
 	DEFAULT_PACKAGE_MAP = map[string]*treeset.Set{
-		// universe is technically a lazy val and so doesn't show up in the maven install
-		// package mapping, but the common pattern is to import from it as if it were a
-		// source package. We just hardcode it as a special case here.
-		"scala.reflect.runtime.universe": treeset.NewWithStringComparator(
-			"@maven//:org_scala_lang_scala_reflect",
-		),
+		// There is nothing here now, but packages may be added if they would otherwise need
+		// to be handled manually by all users. Settings here are impossible for users to
+		// override however, so this should be done with caution.
 	}
 
 	DEFAULT_FORCED_TRANSITIVE_DEPS = map[string][]string{}
